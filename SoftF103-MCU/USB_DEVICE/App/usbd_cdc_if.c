73c334d9a90dcf7c38daff835cb5e01f707af1bb
wuyuepku <wuyue16@pku.edu.cn>
2019-05-22 09:26:37 +0800

    move MCU project

--- SoftF103-MCU/USB_DEVICE/App/usbd_cdc_if.c
+++ SoftF103-MCU/USB_DEVICE/App/usbd_cdc_if.c
@@ -263,6 +263,9 @@ static int8_t CDC_Control_FS(uint8_t cmd, uint8_t* pbuf, uint16_t length)
 static int8_t CDC_Receive_FS(uint8_t* Buf, uint32_t *Len)
 {
   /* USER CODE BEGIN 6 */
+  uint32_t actually_received = fifo_copy_from_buffer(&mem.siorx, (char*)Buf, *Len);
+	mem.siorx_overflow += *Len - actually_received;
+  // CDC_Transmit_FS(Buf, *Len);  // loop test
   USBD_CDC_SetRxBuffer(&hUsbDeviceFS, &Buf[0]);
   USBD_CDC_ReceivePacket(&hUsbDeviceFS);
   return (USBD_OK);
